a9816bec4cfdf002382365699471b23686196e51
alparody <alparody@gmail.com>
2025-08-21 10:28:17 +0300

    Update Indecator.py

--- Indecator.py
+++ Indecator.py
@@ -95,10 +95,9 @@ def push_to_github(content_str, path_in_repo, commit_message="Update weights"):
 
 
 
-@st.cache_data(show_spinner=False)
+# @st.cache_data(show_spinner=False)
 def get_price_data(symbols, start, end):
-    start = pd.to_datetime(start).strftime("%Y-%m-%d")
-    end = pd.to_datetime(end).strftime("%Y-%m-%d")
+    st.write("Fetching data for:", start, end) 
     raw = yf.download(symbols, start=start, end=end, auto_adjust=True, progress=False)["Close"].dropna(how="all", axis=1)
     return raw
 
